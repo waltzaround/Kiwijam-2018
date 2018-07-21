78d5a131584c48414c2113cab241f11550ac88a6
Walter Lim <vxs4050@autuni.ac.nz>
2018-07-21 16:28:17 +1200

    Initial commit

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+
+
+        Quaternion localRotation = Quaternion.Euler(0, rotY, 0.0f);
+        transform.rotation = localRotation;
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, y);
+
+    }
+}
